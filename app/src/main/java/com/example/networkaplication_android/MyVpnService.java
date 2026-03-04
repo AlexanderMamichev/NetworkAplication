bc681136fa4a3773f86d2adcaa55a9748211afe1
Your Name <you@example.com>
2026-03-04 19:22:51 +0100

    making changes to interface buttons and adding comment to the code

--- app/src/main/java/com/example/networkaplication_android/MyVpnService.java
+++ app/src/main/java/com/example/networkaplication_android/MyVpnService.java
@@ -1,3 +1,14 @@
+/*
+ * Copyright 2026 Your Name
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+
 package com.example.networkaplication_android;
 
 import android.content.Intent;
